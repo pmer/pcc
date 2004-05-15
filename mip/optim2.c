e5d7771b65c4271f71a070eaf098206950a94349
ragge <ragge>
2004-05-15 12:14:43 +0000

    Fix gcc 3.3.3 warnings. Check temp/nontemp regs usage in regs.c.

--- mip/optim2.c
+++ mip/optim2.c
@@ -1,4 +1,4 @@
-/*	$Id: optim2.c,v 1.1 2004/05/10 21:35:58 ragge Exp $	*/
+/*	$Id: optim2.c,v 1.2 2004/05/13 19:33:47 ragge Exp $	*/
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -28,6 +28,8 @@
 
 #include "pass2.h"
 #include "external.h"
+
+#include <string.h>
 
 extern int saving;
 
