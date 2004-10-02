b3912ca0f99af696d4560a8e37020c0adda06b9d
ragge <ragge>
2004-10-02 08:01:16 +0000

    Update init stuff.  More bugfixes.

--- arch/i386/local2.c
+++ arch/i386/local2.c
@@ -1,4 +1,4 @@
-/*	$Id: local2.c,v 1.29 2004/06/22 07:55:03 ragge Exp $	*/
+/*	$Id: local2.c,v 1.30 2004/06/22 12:12:36 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -134,7 +134,8 @@ eoftn(int regs, int autos, int retlab)
 	isoptim = 0;
 }
 
-static char *loctbl[] = { "text", "data", "data", "text", "text", "stab" };
+static char *loctbl[] =
+    { "text", "data", "section .rodata", "data", "text", "stab" };
 
 void
 setlocc(int locctr)
