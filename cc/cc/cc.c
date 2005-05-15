ed4b15de757c52816d0aa3004d5772795098c2cd
ragge <ragge>
2005-05-15 12:01:15 +0000

    Make debug printout prettier.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.39 2005/05/15 09:55:02 ragge Exp $	*/
+/*	$Id: cc.c,v 1.40 2005/05/15 10:09:29 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -543,7 +543,8 @@ char f[], *v[]; {
 	char *s;
 
 	if (vflag) {
-		for (t = 0; v[t]; t++)
+		fprintf(stderr, "%s ", f);
+		for (t = 1; v[t]; t++)
 			fprintf(stderr, "%s ", v[t]);
 		fprintf(stderr, "\n");
 	}
