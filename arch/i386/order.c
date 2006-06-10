7f4501bc7f3f3bc05b7e9c1ecd254dc626c8ba38
ragge <ragge>
2006-06-10 15:05:23 +0000

    Change shift left op to always be char. Fix stack removal bug for long long shift calls. Fix long long OPSIMP bug that always became OR.

--- arch/i386/order.c
+++ arch/i386/order.c
@@ -1,4 +1,4 @@
-/*	$Id: order.c,v 1.39 2006/04/01 13:19:15 ragge Exp $	*/
+/*	$Id: order.c,v 1.40 2006/05/30 20:33:20 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -241,7 +241,7 @@ nspecial(struct optab *q)
 	case RS:
 		if (q->visit & (INAREG|INBREG)) {
 			static struct rspecial s[] = {
-				{ NRIGHT, ECX }, { 0 } };
+				{ NRIGHT, CL }, { 0 } };
 			return s;
 		} else if (q->visit & INCREG) {
 			static struct rspecial s[] = {
