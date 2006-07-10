1c0c495be6c311a96eeaae7bba1d4a1930f7a932
ragge <ragge>
2006-07-10 10:05:35 +0000

    Shifts must be promoted to int if smaller type. DIVs converted to RS must have the shift count casted to int. Missed type size in clocal() ICON SCONVs.

--- arch/i386/local.c
+++ arch/i386/local.c
@@ -1,4 +1,4 @@
-/*	$Id: local.c,v 1.42 2006/06/20 06:02:43 ragge Exp $	*/
+/*	$Id: local.c,v 1.43 2006/07/10 07:55:26 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -234,6 +234,7 @@ clocal(NODE *p)
 				cerror("unknown type %d", m);
 			}
 			l->n_type = m;
+			l->n_sue = MKSUE(m);
 			nfree(p);
 			return l;
 		}
