0a2ba7b43d4068e671c718edbf9325d2bb3be1c7
ragge <ragge>
2006-07-15 07:37:20 +0000

    Fix bug causing '\n' and other character constants not to be interpreted. Wrap default buffer size to 64k. XXX - must be fixed.

--- cc/cpp/cpp.h
+++ cc/cpp/cpp.h
@@ -1,4 +1,4 @@
-/*	$Id: cpp.h,v 1.14 2005/05/14 13:28:27 ragge Exp $	*/
+/*	$Id: cpp.h,v 1.15 2005/05/15 21:06:35 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
@@ -70,7 +70,11 @@ struct symtab {
 
 /* buffer used internally */
 #ifndef CPPBUF
+#if 0
 #define CPPBUF  BUFSIZ
+#else
+#define CPPBUF  65536
+#endif
 #endif
 
 #define	NAMEMAX	64 /* max len of identifier */
