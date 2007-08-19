f745e679f99beb1c27cffe0e04fd1eea036c9d97
ragge <ragge>
2007-08-19 19:22:16 +0000

    Fix bug with empty # on a line. Increase default buffers to 64k.

--- cc/cpp/cpp.h
+++ cc/cpp/cpp.h
@@ -1,4 +1,4 @@
-/*	$Id: cpp.h,v 1.23 2006/10/08 13:41:39 ragge Exp $	*/
+/*	$Id: cpp.h,v 1.24 2007/08/08 20:29:14 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
@@ -54,7 +54,11 @@ extern	int	ofd;
 
 /* buffer used internally */
 #ifndef CPPBUF
+#ifdef __pdp11__
 #define CPPBUF  BUFSIZ
+#else
+#define CPPBUF	65536
+#endif
 #endif
 
 #define	NAMEMAX	64 /* max len of identifier */
