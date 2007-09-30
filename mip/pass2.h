4af84fa1aa35d1f0477e74004df846e8f36d97b8
ragge <ragge>
2007-09-30 20:35:40 +0000

    tcopy() is a common routine.

--- mip/pass2.h
+++ mip/pass2.h
@@ -1,4 +1,4 @@
-/*	$Id: pass2.h,v 1.97 2006/06/17 14:23:53 ragge Exp $	*/
+/*	$Id: pass2.h,v 1.98 2006/12/22 06:23:09 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -222,7 +222,6 @@ extern	int p2autooff, p2maxautooff;
 extern	NODE
 	*talloc(void),
 	*eread(void),
-	*tcopy(NODE *),
 	*mklnode(int, CONSZ, int, TWORD),
 	*mkbinode(int, NODE *, NODE *, TWORD),
 	*mkunode(int, NODE *, int, TWORD),
