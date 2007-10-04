8048e5bae91d19d2671412a1198c95ca13275c37
ragge <ragge>
2007-10-04 16:44:32 +0000

    Move compat functions to their own header.  From Laurent Desnogues.

--- cc/cpp/cpp.c
+++ cc/cpp/cpp.c
@@ -1,4 +1,4 @@
-/*	$Id: cpp.c,v 1.64 2007/09/24 15:42:00 ragge Exp $	*/
+/*	$Id: cpp.c,v 1.65 2007/09/25 20:41:08 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
@@ -84,6 +84,7 @@
 #include <alloca.h>
 #endif
 
+#include "../../mip/compat.h"
 #include "cpp.h"
 #include "y.tab.h"
 
