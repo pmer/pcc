b2d2ae1a28f712f5f33893d051206417a4724191
ragge <ragge>
2007-11-12 18:56:25 +0000

    Add funcode() as a function that is called early in buildtree and only once.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.125 2007/09/09 10:01:01 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.126 2007/10/06 15:19:22 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -320,6 +320,7 @@ int cdope(int);
 void myp2tree(NODE *);
 void lcommprint(void);
 void lcommdel(struct symtab *);
+NODE *funcode(NODE *);
 
 #ifdef GCC_COMPAT
 void gcc_init(void);
--- cc/ccom/trees.c
+++ cc/ccom/trees.c
@@ -1,4 +1,4 @@
-/*	$Id: trees.c,v 1.169 2007/10/08 18:27:02 otto Exp $	*/
+/*	$Id: trees.c,v 1.170 2007/11/03 18:55:58 otto Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -484,6 +484,8 @@ runtime:
 
 		case CALL:
 			p->n_right = r = strargs(p->n_right);
+			p = funcode(p);
+			/* FALLTHROUGH */
 		case UCALL:
 			if (!ISPTR(l->n_type))
 				uerror("illegal function");
