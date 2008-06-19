0b80c89015e90aec9a3ac2985a2c346c898d1649
gmcgarry <gmcgarry>
2008-06-19 07:29:22 +0000

    Simulated mod is only good for unsigned integers.  From Stefan Kempf.

--- arch/powerpc/local.c
+++ arch/powerpc/local.c
@@ -1,4 +1,4 @@
-/*	$Id: local.c,v 1.10 2008/04/10 01:34:16 gmcgarry Exp $	*/
+/*	$Id: local.c,v 1.11 2008/05/11 16:55:03 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -35,8 +35,7 @@
 
 extern int kflag;
 
-static
-void simmod(NODE *p);
+static void simmod(NODE *p);
 
 /*	this file contains code which is dependent on the target machine */
 
@@ -1266,6 +1265,9 @@ simmod(NODE *p)
 	NODE *r = p->n_right;
 
 	assert(p->n_op == MOD);
+
+	if (!ISUNSIGNED(p->n_type))
+		return;
 
 #define ISPOW2(n) ((n) && (((n)&((n)-1)) == 0))
 
