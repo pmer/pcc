204b81462374cefba5ded17ea582cc07860549b8
gmcgarry <gmcgarry>
2008-07-16 10:09:57 +0000

    Permit target-defined member to struct ipp using TARGET_IPP_MEMBERS.

--- mip/manifest.h
+++ mip/manifest.h
@@ -1,4 +1,4 @@
-/*	$Id: manifest.h,v 1.78 2008/06/20 12:50:33 gmcgarry Exp $	*/
+/*	$Id: manifest.h,v 1.79 2008/06/29 13:45:10 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -267,6 +267,9 @@ struct interpass_prolog {
 	int ipp_autos;		/* Size on stack needed */
 	int ip_tmpnum;		/* # allocated temp nodes so far */
 	int ip_lblnum;		/* # used labels so far */
+#ifdef TARGET_IPP_MEMBERS
+	TARGET_IPP_MEMBERS
+#endif
 };
 
 /*
