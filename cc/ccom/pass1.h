897026c2df58d6c630847d99567275e694bf06cd
ragge <ragge>
2008-07-28 10:04:24 +0000

    Make inline conform to c99 syntax.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.145 2008/06/20 13:19:03 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.146 2008/06/25 11:50:11 gmcgarry Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -329,6 +329,7 @@ int fldchk(int);
 int nncon(NODE *);
 void cunput(char);
 NODE *nametree(struct symtab *sp);
+void *inlalloc(int size);
 
 
 #ifdef GCC_COMPAT
