3d6b42a786030fdf4901b9c1ecfd5fbcdbc1a7b5
ragge <ragge>
2008-10-09 12:46:01 +0000

    Fix fallouts from shumul arg change.

--- arch/arm/local2.c
+++ arch/arm/local2.c
@@ -1,4 +1,4 @@
-/*      $Id: local2.c,v 1.28 2008/04/14 03:44:37 gmcgarry Exp $    */
+/*      $Id: local2.c,v 1.29 2008/06/22 15:24:59 ragge Exp $    */
 /*
  * Copyright (c) 2007 Gregory McGarry (g.mcgarry@ieee.org).
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
@@ -807,7 +807,7 @@ flshape(NODE *p)
 
 	if (o == OREG || o == REG || o == NAME)
 		return SRDIR; /* Direct match */
-	if (o == UMUL && shumul(p->n_left))
+	if (o == UMUL && shumul(p->n_left, SOREG))
 		return SROREG; /* Convert into oreg */
 	return SRREG; /* put it into a register */
 }
