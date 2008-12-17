1683f9948443bbff13a38cb7f825192779d65c71
ragge <ragge>
2008-12-17 15:59:24 +0000

    Create a type __builtin_va_list for va_list.  Default it is char *, but can be overridden by target if a special type is needed.

--- cc/ccom/main.c
+++ cc/ccom/main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.93 2008/12/08 21:39:50 gmcgarry Exp $	*/
+/*	$Id: main.c,v 1.94 2008/12/13 20:03:32 ragge Exp $	*/
 
 /*
  * Copyright (c) 2002 Anders Magnusson. All rights reserved.
@@ -351,6 +351,15 @@ main(int argc, char *argv[])
 	reached = 1;
 
 	bjobcode();
+#ifndef TARGET_STDARGS
+	{
+		NODE *p = block(NAME, NIL, NIL, PTR|CHAR, NULL, MKSUE(CHAR));
+		struct symtab *sp = lookup(addname("__builtin_va_list"), 0);
+		p->n_sp = sp;
+		defid(p, TYPEDEF);
+		nfree(p);
+	}
+#endif
 
 #ifdef STABS
 	if (gflag) {
