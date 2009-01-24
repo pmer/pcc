5ebe8cbea88a52b7fc220b6d47b541408fb53a98
ragge <ragge>
2009-01-24 15:51:07 +0000

    Add support for weak attribute.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.170 2009/01/14 21:10:01 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.171 2009/01/24 13:35:03 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -386,6 +386,7 @@ enum {	GCC_ATYP_NONE,
 	GCC_ATYP_FORMAT,
 	GCC_ATYP_NONNULL,
 	GCC_ATYP_SENTINEL,
+	GCC_ATYP_WEAK,
 
 	/* other stuff */
 	GCC_ATYP_BOUNDED,	/* OpenBSD extra boundary checks */
