fb12a3bad634312ef8c97b78702180123b17d567
ragge <ragge>
2010-04-28 14:53:54 +0000

    Add support for stdcall and cdecl attributes.  Patch from iain Hibbert.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.190 2010/04/11 15:01:04 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.191 2010/04/18 13:25:18 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -431,6 +431,8 @@ enum {	GCC_ATYP_NONE,
 	GCC_ATYP_CONSTRUCTOR,
 	GCC_ATYP_DESTRUCTOR,
 	GCC_ATYP_VISIBILITY,
+	GCC_ATYP_STDCALL,
+	GCC_ATYP_CDECL,
 
 	/* other stuff */
 	GCC_ATYP_BOUNDED,	/* OpenBSD extra boundary checks */
