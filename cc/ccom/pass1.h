9d76dd3cc1713a798f6876b35c1f7842a4ed703f
ragge <ragge>
2010-06-09 08:09:10 +0000

    Add parsing support for weakref.  While here, cleanup a little in the table for attribute matching.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.196 2010/06/06 19:29:14 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.197 2010/06/09 05:57:08 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -438,6 +438,7 @@ enum {	GCC_ATYP_NONE,
 	GCC_ATYP_NO_INSTR_FUN,
 	GCC_ATYP_NOINLINE,
 	GCC_ATYP_ALIAS,
+	GCC_ATYP_WEAKREF,
 
 	/* other stuff */
 	GCC_ATYP_BOUNDED,	/* OpenBSD extra boundary checks */
