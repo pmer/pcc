2a200dfa44f3a2cc111a86399c2f2b19a29ab0d9
gmcgarry <gmcgarry>
2010-06-15 07:41:24 +0000

    Wrap some more elf-specific stuff that isn't used on OS X.

--- arch/i386/code.c
+++ arch/i386/code.c
@@ -1,4 +1,4 @@
-/*	$Id: code.c,v 1.52 2010/04/28 14:53:54 ragge Exp $	*/
+/*	$Id: code.c,v 1.53 2010/06/09 13:58:22 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -87,6 +87,7 @@ defloc(struct symtab *sp)
 			printf("\t.%s %s\n", ga->a1.sarg, name);
 	}
 #endif
+#ifdef ELFABI
 	if (kflag && !ISFTN(t)) {
 		/* Must place aggregates with pointers in relocatable memory */
 		TWORD t2 = t;
@@ -99,6 +100,7 @@ defloc(struct symtab *sp)
 			s = lastloc = -1;
 		}
 	}
+#endif
 	if (nextsect) {
 		printf("	.section %s,\"wa\",@progbits\n", nextsect);
 		nextsect = NULL;
