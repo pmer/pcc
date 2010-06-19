37fbf9639922369c027e2f7c27683a9ac4a0bda3
ragge <ragge>
2010-06-19 07:37:22 +0000

    Understand gcc attribute always_inline. Fixes part of Jira#PCC-158 by Detlef Riekenberg.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.199 2010/06/15 19:39:43 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.200 2010/06/16 19:41:40 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -441,6 +441,7 @@ enum {	GCC_ATYP_NONE,
 	GCC_ATYP_ALIAS,
 	GCC_ATYP_WEAKREF,
 	GCC_ATYP_ALLOCSZ,
+	GCC_ATYP_ALW_INL,
 
 	/* other stuff */
 	GCC_ATYP_BOUNDED,	/* OpenBSD extra boundary checks */
