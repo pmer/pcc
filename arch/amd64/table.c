d31f6857cc84ae8e370268596dadacaaad291ef8
ragge <ragge>
2010-11-27 16:04:29 +0000

    Fix handling on NaN compares by checking the parity bit after comparision. Simplify x87 compares; on amd64 we can use fucomi instruction which sets the usual status register bits as with xmm instructions. Bugs reported by Nelson H. F. Beebe on pcc-list, and use his test programs to reproduce the bugs.

--- arch/amd64/table.c
+++ arch/amd64/table.c
@@ -1,4 +1,4 @@
-/*	$Id: table.c,v 1.37 2010/11/11 20:50:19 ragge Exp $	*/
+/*	$Id: table.c,v 1.38 2010/11/18 18:12:10 ragge Exp $	*/
 /*
  * Copyright (c) 2008 Michael Shalayeff
  * Copyright (c) 2008 Anders Magnusson (ragge@ludd.ltu.se).
@@ -1322,7 +1322,7 @@ struct optab table[] = {
 { OPLOG,	FORCC,
 	SCREG,	TLDOUBLE,
 	SCREG,	TLDOUBLE,
-		NSPECIAL,	0,
+		0,	RNOP,
 		"ZG", },
 
 { OPLOG,	FORCC,
