a2eb1eba2d5856bc339bddc24a5cf04feddb7c40
ragge <ragge>
2010-12-27 18:45:32 +0000

    Accept empty attributes.

--- cc/ccom/gcc_compat.c
+++ cc/ccom/gcc_compat.c
@@ -1,4 +1,4 @@
-/*      $Id: gcc_compat.c,v 1.65 2010/08/29 13:57:22 ragge Exp $     */
+/*      $Id: gcc_compat.c,v 1.66 2010/11/17 19:48:26 ragge Exp $     */
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -327,6 +327,8 @@ gcc_attribs(NODE *p, void *arg)
 		name = (char *)p->n_sp;
 	} else if (p->n_op == CALL || p->n_op == UCALL) {
 		name = (char *)p->n_left->n_sp;
+	} else if (p->n_op == ICON && p->n_type == STRTY) {
+		return;
 	} else
 		cerror("bad variable attribute");
 
