880d0ff9d9476af58f24ebe93d6fc41c6ca253d9
ragge <ragge>
2011-02-24 22:10:49 +0000

    Let the register names esi and edi be legal as xasm constraints. Fixes Jira#PCC-258 by Piotr Sikora.

--- arch/i386/local2.c
+++ arch/i386/local2.c
@@ -1,4 +1,4 @@
-/*	$Id: local2.c,v 1.153 2011/01/29 09:55:29 ragge Exp $	*/
+/*	$Id: local2.c,v 1.154 2011/02/04 15:08:58 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -1567,6 +1567,8 @@ static struct {
 	{ "ebx", EBX },
 	{ "ecx", ECX },
 	{ "edx", EDX },
+	{ "esi", ESI },
+	{ "edi", EDI },
 	{ "ax", EAX },
 	{ "bx", EBX },
 	{ "cx", ECX },
