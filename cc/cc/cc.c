f1eac8fa2685aec34f4b7a7233da294fb0e12cf0
ragge <ragge>
2011-04-04 14:47:57 +0000

    Support -g0 and add to manpage, patch from Thorsten Glaser. Update some forgotten parameter functionality.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.175 2011/03/24 19:47:14 ragge Exp $	*/
+/*	$Id: cc.c,v 1.176 2011/03/26 06:25:19 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -495,7 +495,10 @@ main(int argc, char *argv[])
 				break;
 
 			case 'g': /* create debug output */
-				gflag++;
+				if (argv[i][2] == '0')
+					gflag = 0;
+				else
+					gflag++;
 				break;
 
 			case 'i':
