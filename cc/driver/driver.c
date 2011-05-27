41006ba8c7dc80ae3804b21a2f289d328bae3fd9
plunky <plunky>
2011-05-27 06:12:56 +0000

    use compiler_flags when compiling

--- cc/driver/driver.c
+++ cc/driver/driver.c
@@ -1,4 +1,4 @@
-/*	$Id: driver.c,v 1.3 2011/05/26 16:49:44 plunky Exp $	*/
+/*	$Id: driver.c,v 1.4 2011/05/27 06:10:32 plunky Exp $	*/
 
 /*-
  * Copyright (c) 2011 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -359,6 +359,7 @@ compile_input(const char *file, char *input, char **output,
 	int retval;
 
 	strlist_init(&args);
+	strlist_append_list(&args, &compiler_flags);
 	if (debug_mode)
 		strlist_append(&args, "-g");
 	if (pic_mode)
