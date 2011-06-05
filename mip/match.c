6f464ce8172f67d9f35bef947f8f2e8c6b5dab2a
plunky <plunky>
2011-06-05 18:51:02 +0000

    ifdef PCC_DEBUG

--- mip/match.c
+++ mip/match.c
@@ -1,4 +1,4 @@
-/*      $Id: match.c,v 1.94 2011/05/04 18:02:32 ragge Exp $   */
+/*      $Id: match.c,v 1.95 2011/06/05 10:19:25 ragge Exp $   */
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -73,7 +73,9 @@ int s2debug;
 
 extern char *ltyp[], *rtyp[];
 
+#ifdef PCC_DEBUG
 static char *srtyp[] = { "SRNOPE", "SRDIR", "SROREG", "SRREG" };
+#endif
 
 /*
  * return true if shape is appropriate for the node p
