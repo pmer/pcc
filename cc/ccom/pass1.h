66006b27bb94b7dc0d1bd4b022ba7cecfdb4dbde
ragge <ragge>
2011-07-14 08:57:43 +0000

    Add x flag -xgcc that tells pcc to use gcc syntax for some things instead of c99 style.  Currently only inline.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.230 2011/06/29 19:46:21 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.231 2011/07/06 20:15:38 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -181,7 +181,7 @@ extern	OFFSZ inoff;
 
 extern	int reached;
 extern	int isinlining;
-extern	int xinline;
+extern	int xinline, xgcc;
 
 extern	int sdebug, idebug, pdebug;
 
