cbd01e9f9eec752e276dc4fd623c419a202dce61
plunky <plunky>
2011-08-28 14:00:13 +0000

    spelling enouth->enough

--- cc/cpp/cpp.h
+++ cc/cpp/cpp.h
@@ -1,4 +1,4 @@
-/*	$Id: cpp.h,v 1.50 2011/07/15 10:52:59 ragge Exp $	*/
+/*	$Id: cpp.h,v 1.51 2011/07/16 16:14:56 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004,2010 Anders Magnusson (ragge@ludd.luth.se).
@@ -61,7 +61,7 @@ extern	int	ofd;
 #endif
 #endif
 
-#define	MAXARGS	128	/* Max # of args to a macro. Should be enouth */
+#define	MAXARGS	128	/* Max # of args to a macro. Should be enough */
 
 #define	NAMEMAX	CPPBUF	/* currently pushbackbuffer */
 #define	BBUFSZ	(NAMEMAX+CPPBUF+1)
