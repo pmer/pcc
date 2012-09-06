152fe2ab5995a30c95eac6af6c24d822b54db5b0
plunky <plunky>
2012-09-06 20:51:14 +0000

    be type consistent, and use size_t for allocation functions

--- cc/ccom/main.c
+++ cc/ccom/main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.118 2012/03/22 18:51:40 plunky Exp $	*/
+/*	$Id: main.c,v 1.119 2012/08/09 11:41:27 ragge Exp $	*/
 
 /*
  * Copyright (c) 2002 Anders Magnusson. All rights reserved.
@@ -343,15 +343,16 @@ main(int argc, char *argv[])
 void
 prtstats(void)
 {
-	extern int nametabs, namestrlen, tmpallocsize, permallocsize;
-	extern int lostmem, arglistcnt, dimfuncnt, inlnodecnt, inlstatcnt;
+	extern int nametabs, namestrlen;
+	extern int arglistcnt, dimfuncnt, inlnodecnt, inlstatcnt;
 	extern int symtabcnt, suedefcnt;
+	extern size_t permallocsize, tmpallocsize, lostmem;
 
 	fprintf(stderr, "Name table entries:		%d pcs\n", nametabs);
 	fprintf(stderr, "Name string size:		%d B\n", namestrlen);
-	fprintf(stderr, "Permanent allocated memory:	%d B\n", permallocsize);
-	fprintf(stderr, "Temporary allocated memory:	%d B\n", tmpallocsize);
-	fprintf(stderr, "Lost memory:			%d B\n", lostmem);
+	fprintf(stderr, "Permanent allocated memory:	%zu B\n", permallocsize);
+	fprintf(stderr, "Temporary allocated memory:	%zu B\n", tmpallocsize);
+	fprintf(stderr, "Lost memory:			%zu B\n", lostmem);
 	fprintf(stderr, "Argument list unions:		%d pcs\n", arglistcnt);
 	fprintf(stderr, "Dimension/function unions:	%d pcs\n", dimfuncnt);
 	fprintf(stderr, "Struct/union/enum blocks:	%d pcs\n", suedefcnt);
