f56946b4933d3a9f50cf465f3d126ccb6501bdc6
plunky <plunky>
2012-10-22 09:14:42 +0000

    for strictness, have a break here (fallthrough would have no effect but was unintentional)

--- cc/ccom/trees.c
+++ cc/ccom/trees.c
@@ -1,4 +1,4 @@
-/*	$Id: trees.c,v 1.316 2012/09/26 19:56:12 plunky Exp $	*/
+/*	$Id: trees.c,v 1.317 2012/10/22 08:55:33 plunky Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -1079,6 +1079,7 @@ chkpun(NODE *p)
 			uerror("using void value");
 			return;
 		}
+		break;
 	case COLON:
 		if (t1 == VOID && t2 == VOID)
 			return;
