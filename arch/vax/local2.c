d552806998c3ea58f7aa9bd888fc5e8215b5fb36
ragge <ragge>
2014-10-12 09:59:31 +0000

    rmove() must understand long long, which did not exist when this code was written.

--- arch/vax/local2.c
+++ arch/vax/local2.c
@@ -1,4 +1,4 @@
-/*	$Id: local2.c,v 1.38 2014/10/10 16:08:40 ragge Exp $	*/
+/*	$Id: local2.c,v 1.39 2014/10/11 15:57:17 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -665,9 +665,9 @@ zzzcode(NODE *p, int c)
 void
 rmove(int rt, int rs, TWORD t)
 {
-	printf( "	%s	%s,%s\n",
-		(t==FLOAT ? "movf" : (t==DOUBLE ? "movd" : "movl")),
-		rnames[rt], rnames[rs] );
+	char c = (t == FLOAT ? 'f' : t == DOUBLE ? 'd' :
+	    t == LONGLONG || t == ULONGLONG ? 'q' : 'l');
+	printf("	mov%c	%s,%s\n", c, rnames[rt], rnames[rs]);
 }
 
 int
