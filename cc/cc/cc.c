c3a74e448db3db9912db411435ee1f7987567402
ragge <ragge>
2014-11-11 07:30:49 +0000

    " failed\n" is 8 bytes long.  From Alan Cox.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.282 2014/10/10 16:15:08 ragge Exp $	*/
+/*	$Id: cc.c,v 1.283 2014/10/11 10:50:26 ragge Exp $	*/
 
 /*-
  * Copyright (c) 2011 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -1317,7 +1317,7 @@ strlist_exec(struct strlist *l)
 		execvp(argv[0], argv);
 		result = write(STDERR_FILENO, "Exec of ", 8);
 		result = write(STDERR_FILENO, argv[0], strlen(argv[0]));
-		result = write(STDERR_FILENO, " failed\n", 7);
+		result = write(STDERR_FILENO, " failed\n", 8);
 		(void)result;
 		_exit(127);
 	case -1:
