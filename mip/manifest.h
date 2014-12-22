3c4d934b43f063c4d14dc8c064a83af66225d8ef
plunky <plunky>
2014-12-22 22:07:40 +0000

    use const for warning function prototypes (reduce warnings)
    fixes PCC-506 by Antoine Leca

--- mip/manifest.h
+++ mip/manifest.h
@@ -1,4 +1,4 @@
-/*	$Id: manifest.h,v 1.103 2014/06/06 13:19:03 plunky Exp $	*/
+/*	$Id: manifest.h,v 1.104 2014/06/07 07:04:10 plunky Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -346,9 +346,9 @@ void fwalk(NODE *t, void (*f)(NODE *, int, int *, int *), int down);
 void flist(NODE *p, void (*f)(NODE *, void *), void *);
 void listf(NODE *p, void (*f)(NODE *));
 NODE *listarg(NODE *p, int n, int *cnt);
-void cerror(char *s, ...);
-void werror(char *s, ...);
-void uerror(char *s, ...);
+void cerror(const char *s, ...);
+void werror(const char *s, ...);
+void uerror(const char *s, ...);
 void mkdope(void);
 void tcheck(void);
 
