7a0ccff8ab4fd513b984f76d36b6063f4e58318a
ragge <ragge>
2015-09-28 16:25:24 +0000

    Restore input pointer after searching for a ( that do not exist. Fixes bug reported by Iain Hibbert on pcc-list.

--- cc/cpp/cpp.c
+++ cc/cpp/cpp.c
@@ -1,4 +1,4 @@
-/*	$Id: cpp.c,v 1.233 2015/09/04 05:50:22 ragge Exp $	*/
+/*	$Id: cpp.c,v 1.234 2015/09/20 16:18:13 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004,2010 Anders Magnusson (ragge@ludd.luth.se).
@@ -1397,12 +1397,14 @@ sstr:				for (; cp < ib->cptr; cp++)
 				goto sstr;
 			} else {
 				if (*sp->value != OBJCT) {
+					cp = ib->cptr;
 					while (ISWS(*ib->cptr))
 						ib->cptr++;
 					if (*ib->cptr == 0) {
 						bufree(xb);
 						return sp;
 					}
+					ib->cptr = cp;
 				}
 newmac:				if ((xob = submac(sp, 1, ib, NULL)) == NULL) {
 					savstr(sp->namep);
@@ -1560,10 +1562,13 @@ submac(struct symtab *sp, int lvl, struct iobuf *ib, struct blocker *obl)
 		DPRINT(("%d:submac: return exparg\n", lvl));
 		break;
 	default:
+		cp = ib->cptr;
 		while (ISWSNL(*ib->cptr))
 			ib->cptr++;
-		if (*ib->cptr != '(')
+		if (*ib->cptr != '(') {
+			ib->cptr = cp;
 			return 0;
+		}
 		cp = ib->cptr++;
 		pr = stringbuf;
 		if (readargs2(&ib->cptr, sp, argary)) {
