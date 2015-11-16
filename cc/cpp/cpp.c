cea90842654d6adb13d934b1ee7f8d4c63832292
ragge <ragge>
2015-11-16 21:04:02 +0000

    Fix problem with creating new tokens, same as token.c:1.155 but when parsing argument lists.  Reported by Iain Hibbert on pcc-list.

--- cc/cpp/cpp.c
+++ cc/cpp/cpp.c
@@ -1,4 +1,4 @@
-/*	$Id: cpp.c,v 1.246 2015/11/14 13:49:25 ragge Exp $	*/
+/*	$Id: cpp.c,v 1.247 2015/11/14 17:02:07 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004,2010 Anders Magnusson (ragge@ludd.luth.se).
@@ -2126,7 +2126,13 @@ sstr:				for (; bp < cp; bp++)
 			stringbuf = sbp;
 			if (expokb(nl, bl) && expok(nl, m)) {
 				if ((nob = submac(nl, lvl+1, ib, bl))) {
+					if (nob->buf[0] == '-' ||
+					    nob->buf[0] == '+')
+						putob(ob, ' ');
 					strtobuf(nob->buf, ob);
+					if (ob->cptr[-1] == '-' ||
+					    ob->cptr[-1] == '+')
+						putob(ob, ' ');
 					bufree(nob);
 				} else {
 					goto sblk;
