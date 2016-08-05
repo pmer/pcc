c84ba948e195fb391524596e6fbdd7825e08632c
ragge <ragge>
2016-08-05 10:23:31 +0000

    Do not print out multiple \n, if more than 10 then print a line directive instead.  Also skip printing out leading whitespace on each line. This reduces the size of intermediate file significantly and also speed up the compiler quite much.

--- cc/cpp/cpp.h
+++ cc/cpp/cpp.h
@@ -1,4 +1,4 @@
-/*	$Id: cpp.h,v 1.104 2016/05/15 10:15:04 ragge Exp $	*/
+/*	$Id: cpp.h,v 1.105 2016/05/16 18:43:50 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004,2010 Anders Magnusson (ragge@ludd.luth.se).
@@ -212,3 +212,4 @@ int fastnum(int ch, struct iobuf *);
 void *xrealloc(void *p, int sz);
 void *xmalloc(int sz);
 void fastscan(void);
+void cntline(void);
