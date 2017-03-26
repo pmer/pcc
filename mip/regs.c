f98d1793c468953c3d2793dbe609570f7f0307a7
ragge <ragge>
2017-03-26 18:14:14 +0000

    Ensure that the rnames array is the size of all registers, and that NULL entries do not affect xasm checking. Bugfix given from M. Levinson.

--- mip/regs.c
+++ mip/regs.c
@@ -1,4 +1,4 @@
-/*	$Id: regs.c,v 1.249 2016/10/11 13:48:24 ragge Exp $	*/
+/*	$Id: regs.c,v 1.250 2017/03/11 09:22:09 ragge Exp $	*/
 /*
  * Copyright (c) 2005 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -1448,7 +1448,7 @@ xasmconstr(NODE *p, void *arg)
 		return;
 
 	for (i = 0; i < MAXREGS; i++)
-		if (strcmp(rnames[i], p->n_name) == 0) {
+		if (rnames[i] && strcmp(rnames[i], p->n_name) == 0) {
 			addalledges(&ablock[i]);
 			return;
 		}
