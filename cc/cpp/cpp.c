69e1b77de799cdc3b5f2ae371061b260e2f41d3e
ragge <ragge>
2017-06-24 14:19:03 +0000

    Inline putob() on two places to speed up processing (due to gprof).

--- cc/cpp/cpp.c
+++ cc/cpp/cpp.c
@@ -1,4 +1,4 @@
-/*	$Id: cpp.c,v 1.300 2017/06/23 10:30:27 ragge Exp $	*/
+/*	$Id: cpp.c,v 1.301 2017/06/24 10:42:06 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004,2010 Anders Magnusson (ragge@ludd.luth.se).
@@ -78,6 +78,8 @@ static void prrep(mvtyp);
 #else
 #define DPRINT(x)
 #endif
+#define	PUTOB(ob, ch) (ob->cptr == ob->bsz ? \
+	putob(ob, ch) : (void)(ob->buf[ob->cptr++] = ch))
 
 static int istty;
 int Aflag, Cflag, Eflag, Mflag, dMflag, Pflag, MPflag, MMDflag;
@@ -527,7 +529,7 @@ strtobuf(const usch *str, struct iobuf *iob)
 		iob = getobuf(BNORMAL);
 	DPRINT(("strtobuf iob %p buf %p str %s\n", iob, iob->buf, str));
 	do {
-		putob(iob, *str);
+		PUTOB(iob, *str);
 	} while (*str++);
 	iob->cptr--;
 	return iob;
@@ -2281,7 +2283,7 @@ exparg(int lvl, struct iobuf *ib, struct iobuf *ob, struct blocker *bl)
 			break;
 
 		default:
-			putob(ob, c);
+			PUTOB(ob, c);
 			ib->cptr++;
 			break;
 		}
